e25aad267cbf370da69d343ff1a440b4c0de0942
kacperrutkowski <kcprutkowski@gmail.com>
2026-02-01 13:26:33 +0100

    Dokończenie funkcji sprawdz_kolumny

--- src/walidacja_danych.py
+++ src/walidacja_danych.py
@@ -9,13 +9,20 @@ def sprawdz_kolumny(df: DataFrame) -> bool:
         return True
 
     if df.columns[0] == ('dzień miesiąca', 'dzień tygodnia'):
-        if  28 <= len(df.columns) <= 31:
-            #dni_tygodnia = ['pn', 'wt', 'śr', 'cz', 'pt', 'sb', 'nd']
+        #print('Dni miesiąca zgadzają się')
+        if  29 <= len(df.columns) <= 32: #od 29 do 32, bo na początku jest jeszcze kolumna z ('dzień miesiąca', 'dzień tygodnia')
+            #print('ilość dni miesiąca zgadza się')
+            week_days = ['pn', 'wt', 'śr', 'cz', 'pt', 'sb', 'nd']
             for i in range(2,len(df.columns)):
-                cond1 = (df.columns[i-1] + 1) == df.columns[i]
-                if not cond1:
+                prev_day = df.columns[i-1]
+                curr_day = df.columns[i]
+                cond_month_days = prev_day[0] == (curr_day[0] - 1)
+                cond_week_days = prev_day[1] ==  week_days[week_days.index(curr_day[1]) - 1]
+                if not (cond_month_days & cond_week_days):
+                    #print(f'cond_month_days: {cond_month_days}, cond_week_days: {cond_week_days}')
                     return False
             return True
+    #print('Final False')
     return False
 
 
