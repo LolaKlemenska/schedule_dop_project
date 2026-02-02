973198df704b1094a056fa843200af510a2fa480
Dell <stas.piskorski@gmail.com>
2026-02-02 19:52:03 +0100

    Poprawienie funkcji sprawdz_kolumny, dodanie funkcji sprawdz_typy_danych, sprawdz_kolumny_i_typy

--- src/walidacja_danych.py
+++ src/walidacja_danych.py
@@ -1,29 +1,20 @@
 import pandas as pd
 
 
-def sprawdz_kolumny(df: pd.DataFrame) -> bool:
-    dozwolone_schematy = [['pracownik', 'specjalizacja', 'nazwa_zajec', 'rola', 'udział'],
-                          ['dzien', 'czas', 'sala', 'nazwa_zajec']]
+def sprawdz_kolumny(df: pd.DataFrame):
+    schematy = {
+        "umiejetnosci": ['pracownik', 'specjalizacja', 'nazwa_zajec', 'rola', 'udział'],
+        "rozklad": ['dzien', 'czas', 'sala', 'nazwa_zajec'],
+        "dyspozycyjnosc": ["pracownik", "dzień_miesiąca", "dzień_tygodnia", "godziny"],
+        "rozklad_miesiac": ["dzien_miesiaca", "dzien_tygodnia", "czas", "sala", "nazwa_zajec"],
+        "kalendarz": ["dzien_miesiaca", "dzien_tygodnia"]
+    }
 
-    if list(df.columns) in dozwolone_schematy:
-        return True
+    for nazwa, kolumny in schematy.items():
+        if list(df.columns) == kolumny:
+            return nazwa
 
-    if df.columns[0] == ('dzień miesiąca', 'dzień tygodnia'):
-        #print('Dni miesiąca zgadzają się')
-        if  29 <= len(df.columns) <= 32: #od 29 do 32, bo na początku jest jeszcze kolumna z ('dzień miesiąca', 'dzień tygodnia')
-            #print('ilość dni miesiąca zgadza się')
-            week_days = ['pn', 'wt', 'śr', 'cz', 'pt', 'sb', 'nd']
-            for i in range(2,len(df.columns)):
-                prev_day = df.columns[i-1]
-                curr_day = df.columns[i]
-                cond_month_days = prev_day[0] == (curr_day[0] - 1)
-                cond_week_days = prev_day[1] ==  week_days[week_days.index(curr_day[1]) - 1]
-                if not (cond_month_days & cond_week_days):
-                    #print(f'cond_month_days: {cond_month_days}, cond_week_days: {cond_week_days}')
-                    return False
-            return True
-    #print('Final False')
-    return False
+    return None
 
 def sprawdz_NaN(df: pd.DataFrame, nazwa: str = "DataFrame") -> list[str]:
     """Sprawdza występowanie wartości NaN."""
@@ -37,5 +28,77 @@ def sprawdz_NaN(df: pd.DataFrame, nazwa: str = "DataFrame") -> list[str]:
         )
     if bledy == []:
         return None
+    return bledy
+
+def sprawdz_typy_danych(
+    df: pd.DataFrame,
+    oczekiwane_typy: dict[str, type],
+    nazwa: str = "DataFrame"
+) -> list[str]:
+    """
+    oczekiwane_typy = {
+        'kolumna': typ (np. int, float, str)
+    }
+    """
+    bledy = []
+
+    for kol, typ in oczekiwane_typy.items():
+        if kol not in df.columns:
+            bledy.append(f"[{nazwa}] Brak kolumny '{kol}'.")
+            continue
+
+        if not df[kol].map(lambda x: isinstance(x, typ) or pd.isna(x)).all():
+            bledy.append(
+                f"[{nazwa}] Kolumna '{kol}' nie ma typu {typ.__name__}."
+            )
+
+    return bledy
+
+def sprawdz_kolumny_i_typy(df: pd.DataFrame) -> list[str]:
+    bledy = []
+
+    SCHEMAT_TYPY = {
+    "umiejetnosci": {
+        "pracownik": str,
+        "specjalizacja": str,
+        "nazwa_zajec": str,
+        "rola": str,
+        "udział": (int, float)
+    },
+    "rozklad": {
+        "dzien": str,
+        "czas": str,
+        "sala": str,
+        "nazwa_zajec": str
+    },
+    "dyspozycyjnosc": {
+        "pracownik": str,
+        "dzień_miesiąca": int,
+        "dzień_tygodnia": str,
+        "godziny": (int, float)
+    },
+    "rozklad_miesiac": {
+        "dzien_miesiaca": int,
+        "dzien_tygodnia": str,
+        "czas": str,
+        "sala": str,
+        "nazwa_zajec": str
+    },
+    "kalendarz": {
+        "dzien_miesiaca": int,
+        "dzien_tygodnia": str
+        }
+    }
+    schemat = sprawdz_kolumny(df)
+    if schemat is None:
+        bledy.append("Nieznany schemat kolumn DataFrame.")
+        return bledy
+
+    bledy += sprawdz_typy_danych(
+        df,
+        SCHEMAT_TYPY[schemat],
+        nazwa=schemat
+    )
+
     return bledy
 
