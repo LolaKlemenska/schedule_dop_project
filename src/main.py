98d80497c7a03167e400e2db44f5776f241aaff3
Dell <stas.piskorski@gmail.com>
2026-02-03 18:13:48 +0100

    Zaktualizowanie pipelineu

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from wczytywanie_danych import wczytaj_umiejetnosci, wczytaj_rozklad_zajec, wczytaj_dyspozycyjnosc, wczytaj_kalendarz, wczytaj_rozklad_zajec_miesiac
-from walidacja_danych import sprawdz_kolumny_i_typy
+from walidacja_danych import waliduj_df
 from generowanie_harmonogramu import przygotuj_dane, generuj_liste_dostepnych_prac, generuj_liste_kompetentnych_pracowników, generuj_osobnika
 import os
 
@@ -14,17 +14,19 @@ def pipeline():
     '''Walidacja poprawności wgranych plików'''
     print('Waliduję przesłane pliki...')
     i = 0
-    if len(sprawdz_kolumny_i_typy(umiejetnosci)) < 1:
+    if len(waliduj_df(umiejetnosci)) < 1:
         print("Plik z umiejętnościami wgrany poprawnie")
         i = i+1
     else:
         print("Plik umiejętności zawiera błędy")
-    if len(sprawdz_kolumny_i_typy(rozklad_zajec_miesiac)) < 1:
+    if len(waliduj_df(rozklad_zajec_miesiac)) < 1:
         print("Plik z rozkładem zajęć wgrany poprawnie")
         i = i+1
     else:
         print("Plik rozkład zajęć zawiera błędy")
-    if len(sprawdz_kolumny_i_typy(dyspozycyjnosc)) < 1:
+        print(waliduj_df(rozklad_zajec_miesiac))
+
+    if len(waliduj_df(dyspozycyjnosc)) < 1:
         print("Plik z dyspozycyjnością wgrany poprawnie")
         i = i+1
     else:
