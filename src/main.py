6fcc95c47415fd3ab83c83cb7de57b5628aa39fe
Lola <k.klemenska.509@studms.ug.edu.pl>
2026-02-04 23:58:11 +0100

    dodanie walidacji danych wejściowych
    do funkcji pipeline

--- src/main.py
+++ src/main.py
@@ -11,6 +11,30 @@ def pipeline():
     kalendarz = wczytaj_kalendarz(os.path.join("..", "data", "grafik.xlsx"))
     rozklad_miesiac = wczytaj_rozklad_zajec_miesiac(os.path.join("..", "data", "zajęcia.xlsx"), os.path.join("..", "data", "grafik.xlsx"))
     print("Wgrano pliki wejsciowe")
+    '''Walidacja poprawności wgranych plików'''
+    print('Waliduję przesłane pliki...')
+    i = 0
+    if len(sprawdz_kolumny_i_typy(umiejetnosci)) < 1:
+        print("Plik z umiejętnościami wgrany poprawnie")
+        i = i+1
+    else:
+        print("Plik umiejętności zawiera błędy")
+    if len(sprawdz_kolumny_i_typy(rozklad_zajec_miesiac)) < 1:
+        print("Plik z rozkładem zajęć wgrany poprawnie")
+        i = i+1
+    else:
+        print("Plik rozkład zajęć zawiera błędy")
+    if len(sprawdz_kolumny_i_typy(dyspozycyjnosc)) < 1:
+        print("Plik z dyspozycyjnością wgrany poprawnie")
+        i = i+1
+    else:
+        print("Plik dyspozycyjność zawiera błędy")
+    #sprawdz_kolumny_i_typy(rozklad_miesiac)
+    #sprawdz_kolumny_i_typy(kalendarz)
+    if i == 3:
+        print("Wszystkie wgrane pliki są poprawne")
+    else:
+        print("Wgrane pliki zawierają błędy")
     print("Hello from zaliczenie-projekt!")
 
 if __name__ == "__main__":
