9cb1c67d0f6bb7cc5da74e466f35754434253ce9
kacperrutkowski <kcprutkowski@gmail.com>
2026-02-04 22:52:38 +0100

    Poprawienie parametrów przeprowadz_ewolucje

--- src/generowanie_harmonogramu.py
+++ src/generowanie_harmonogramu.py
@@ -224,9 +224,9 @@ def przeprowadz_ewolucje(
         df_umiejetnosci: pd.DataFrame,
         df_rozklad_zajec_miesiac: pd.DataFrame,
         df_dyspozycyjnosc: pd.DataFrame,
-        fitness_limit= 350,
-        limit_generacji= 500,
-        rozmiar_populacji= 25
+        fitness_limit=400,
+        limit_generacji=500,
+        rozmiar_populacji=30
 ) -> tuple[Populacja, int, list]:
     umiejetnosci, zajecia, dyspozycyjnosc, id_pracownikow = przygotuj_dane(df_umiejetnosci, df_rozklad_zajec_miesiac, df_dyspozycyjnosc)
     populacja = generuj_populacje(rozmiar_populacji, umiejetnosci, zajecia, dyspozycyjnosc, id_pracownikow)
